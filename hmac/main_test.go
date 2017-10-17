edc41db85026b1fe44950e0c3cd1858f8a58f461
fredhw <fredhw@uw.edu>
2017-10-17 15:22:52 -0700

    10/17

--- hmac/main_test.go
+++ hmac/main_test.go
@@ -1,12 +1,36 @@
 package main
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestSign(t *testing.T) {
 	//TODO: write unit test cases for sign()
 	//use strings.NewReader() to get an io.Reader
 	//interface over a simple string
 	//https://golang.org/pkg/strings/#NewReader
+
+	cases := []struct {
+		input          string
+		signingKey     string
+		expectedOutput string
+	}{
+		{
+			input:          "a value to sign",
+			signingKey:     "testkey",
+			expectedOutput: "ICsvg9vnLmvwrdnLdj-U-ssYCTYkaLm82mMM8DACQfU=",
+		},
+	}
+	for _, c := range cases {
+		output, err := sign(c.signingKey, strings.NewReader(c.input))
+		if err != nil {
+			t.Errorf("error signing: %v", err)
+		}
+		if output != c.expectedOutput {
+			t.Errorf("expected %s but go %s", c.expectedOutput, output)
+		}
+	}
 }
 
 func TestVerify(t *testing.T) {
