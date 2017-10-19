629e72140bfbbec0c8265467d62e553bf03f0e6c
fredhw <fredhw@uw.edu>
2017-10-19 15:28:18 -0700

    10/19

--- tasksvr/handlers/tasks.go
+++ tasksvr/handlers/tasks.go
@@ -1,6 +1,11 @@
 package handlers
 
 import (
+	"path"
+	"gopkg.in/mgo.v2/bson"
+	"encoding/json"
+	"github.com/fredhw/info344-in-class/tasksvr/models/tasks"
+	"fmt"
 	"net/http"
 )
 
@@ -10,9 +15,26 @@ func (ctx *Context) TasksHandler(w http.ResponseWriter, r *http.Request) {
 	case "GET":
 		//TODO: parse the `completed` query string param as a boolean
 		//and call the GetAll() method on the tasks.Store to get the tasks
+		tasks, err := ctx.tasksStore.GetAll(false)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("error getting tasks: %v", err), http.StatusInternalServerError)
+		}
+		respond(w, tasks)
 	case "POST":
 		//TODO: decode the request body into a tasks.NewTask
 		//and insert it using the tasks.Store
+		nt := &tasks.NewTask{}
+		if err := json.NewDecoder(r.Body).Decode(nt); err != nil {
+			http.Error(w, fmt.Sprintf("error decoding JSON: %v", err), http.StatusBadRequest)
+			return
+		}
+
+		task, err := ctx.tasksStore.Insert(nt)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("error inserting task: %v", err), http.StatusInternalServerError)
+			return
+		}
+		respond(w, task)
 	default:
 		http.Error(w, "method must be GET or POST", http.StatusMethodNotAllowed)
 		return
@@ -21,11 +43,24 @@ func (ctx *Context) TasksHandler(w http.ResponseWriter, r *http.Request) {
 
 //SpecificTaskHandler handles requests for the /v1/tasks/...task-id... resource
 func (ctx *Context) SpecificTaskHandler(w http.ResponseWriter, r *http.Request) {
+	id := path.Base(r.URL.Path)
+	oid := bson.ObjectIdHex(id)
 	switch r.Method {
 	case "PATCH":
 		//TODO: decode the request body into a
 		//tasks.TaskUpdates struct and pass that
 		//to the Update() method on the tasks.Store
+		tu := &tasks.TaskUpdates{}
+		if err := json.NewDecoder(r.Body).Decode(tu); err != nil {
+			http.Error(w, fmt.Sprintf("error decoding JSON: %v", err), http.StatusBadRequest)
+			return
+		}
+		task, err := ctx.tasksStore.Update(oid, tu)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("error udating task: %v", err), http.StatusInternalServerError)
+			return
+		}
+		respond(w, task)
 	default:
 		http.Error(w, "method must be PATCH", http.StatusMethodNotAllowed)
 		return
